bd340b2f954dbb356bcfc10d6c85cb3f443b6a04
Weidong Xu <weidxu@microsoft.com>
2020-10-29 17:24:46 +0800

    add test

--- test/test/src/test/java/com/azure/frontdoor/FrontDoorTests.java
+++ test/test/src/test/java/com/azure/frontdoor/FrontDoorTests.java
@@ -3,13 +3,22 @@ package com.azure.frontdoor;
 import com.azure.Base;
 import com.azure.core.http.policy.HttpLogDetailLevel;
 import com.azure.core.http.policy.HttpLogOptions;
+import com.azure.core.management.SubResource;
 import com.azure.resourcemanager.frontdoor.generated.FrontDoorManager;
 import com.azure.resourcemanager.frontdoor.generated.fluent.models.FrontendEndpointInner;
 import com.azure.resourcemanager.frontdoor.generated.models.Backend;
 import com.azure.resourcemanager.frontdoor.generated.models.BackendEnabledState;
 import com.azure.resourcemanager.frontdoor.generated.models.BackendPool;
+import com.azure.resourcemanager.frontdoor.generated.models.ForwardingConfiguration;
 import com.azure.resourcemanager.frontdoor.generated.models.FrontDoor;
+import com.azure.resourcemanager.frontdoor.generated.models.FrontDoorForwardingProtocol;
+import com.azure.resourcemanager.frontdoor.generated.models.FrontDoorHealthProbeMethod;
+import com.azure.resourcemanager.frontdoor.generated.models.FrontDoorProtocol;
+import com.azure.resourcemanager.frontdoor.generated.models.HealthProbeEnabled;
+import com.azure.resourcemanager.frontdoor.generated.models.HealthProbeSettingsModel;
+import com.azure.resourcemanager.frontdoor.generated.models.LoadBalancingSettingsModel;
 import com.azure.resourcemanager.frontdoor.generated.models.RoutingRule;
+import com.azure.resourcemanager.frontdoor.generated.models.RoutingRuleEnabledState;
 import com.azure.resourcemanager.frontdoor.generated.models.SessionAffinityEnabledState;
 import com.azure.resourcemanager.storage.generated.StorageManager;
 import com.azure.resourcemanager.storage.generated.models.Kind;
@@ -18,6 +27,7 @@ import com.azure.resourcemanager.storage.generated.models.SkuName;
 import com.azure.resourcemanager.storage.generated.models.StorageAccount;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 public class FrontDoorTests extends Base {
@@ -54,7 +64,7 @@ public class FrontDoorTests extends Base {
                 .withFrontendEndpoints(Collections.singletonList(
                         new FrontendEndpointInner()
                                 .withName("frontend1")
-                                .withHostname(fdName)
+                                .withHostname("fd1weidxu.azurefd.net")
                                 .withSessionAffinityEnabledState(SessionAffinityEnabledState.DISABLED)
                 ))
                 .withBackendPools(Collections.singletonList(
@@ -68,9 +78,37 @@ public class FrontDoorTests extends Base {
                                         .withPriority(1)
                                         .withWeight(50)
                         ))
+                        .withLoadBalancingSettings(new SubResource().withId())
+                ))
+                .withLoadBalancingSettings(Collections.singletonList(
+                        new LoadBalancingSettingsModel()
+                            .withName("loadbalancer1")
+                            .withSampleSize(4)
+                            .withSuccessfulSamplesRequired(2)
+                            .withAdditionalLatencyMilliseconds(0)
+                ))
+                .withHealthProbeSettings(Collections.singletonList(
+                        new HealthProbeSettingsModel()
+                                .withName("healthprobe1")
+                                .withEnabledState(HealthProbeEnabled.ENABLED)
+                                .withPath("/")
+                                .withProtocol(FrontDoorProtocol.HTTPS)
+                                .withHealthProbeMethod(FrontDoorHealthProbeMethod.HEAD)
+                                .withIntervalInSeconds(30)
+                ))
+                .withRoutingRules(Collections.singletonList(
+                        new RoutingRule()
+                                .withName("rule1")
+                                .withEnabledState(RoutingRuleEnabledState.ENABLED)
+                                .withFrontendEndpoints(Collections.singletonList(new SubResource().withId()))
+                                .withAcceptedProtocols(Arrays.asList(FrontDoorProtocol.HTTP, FrontDoorProtocol.HTTPS))
+                                .withPatternsToMatch("/*")
+                                .withRouteConfiguration(new ForwardingConfiguration()
+                                        .withForwardingProtocol(FrontDoorForwardingProtocol.HTTPS_ONLY)
+                                        .withBackendPool(new SubResource().withId()))
                 ))
                 .create();
 
-        int i = 1;
+        frontDoor.refresh();
     }
 }
