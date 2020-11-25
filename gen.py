620cbe9cb870d52c847eb5a6b19fb61146de6d32
Weidong Xu <weidxu@microsoft.com>
2020-11-25 10:48:17 +0800

    remove redundant option override

--- gen.py
+++ gen.py
@@ -58,7 +58,7 @@ def generate(config: dict, **kwargs):
     shutil.rmtree(output_dir, ignore_errors = True)
     tag = '--tag={0}'.format(config.get('tag')) if config.get('tag') else ''
     sdk_integration = '--sdk-integration' if kwargs.get('sdk') else ''
-    command = 'autorest --version={0} --use={1} --java.azure-libraries-for-java-folder={2} --java.output-folder={3} --payload-flattening-threshold=0 --pipeline.modelerfour.flatten-payloads=false --verbose --java.namespace={4} {5}'.format(
+    command = 'autorest --version={0} --use={1} --java.azure-libraries-for-java-folder={2} --java.output-folder={3} --pipeline.modelerfour.flatten-payloads=false --verbose --java.namespace={4} {5}'.format(
         AUTOREST_CORE_VERSION,
         kwargs['use'],
         os.path.abspath(kwargs['output']),
